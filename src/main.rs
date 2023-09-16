19216c664e3317b24aacc839bf9f2c5c40281b5a
aboosoyeed <soyeed2000@gmail.com>
2023-09-16 15:40:44 +0930

    test engine

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,23 @@ mod components;
 mod utils;
 use pgn::PGN;
 
-
-
+mod engine{
+    pub mod engine;
+}
+use engine::engine::Engine;
 
 fn main() {
 
-    
+    let mut engine = Engine::new();
     let fens = PGN::parse("./test.pgn");
 
     for (i,f) in fens.iter().enumerate(){
-        println!("{}. {}",(i+1),f)
+        //println!("{}. {}",(i+1),f);
+        let best_move = engine.process_fen(&f);
+        println!("{}. Best move: {}", i, best_move);
     }    
-    
+    // Terminate the engine process
+    engine.quit();
 }
 
 
